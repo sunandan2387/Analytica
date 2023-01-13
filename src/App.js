e177f1826c139d1ad0f24ea7903e45fe8af5cec8
sunandan <sunandanlalka@gmail.com>
2023-01-13 10:23:28 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Discover from "./screens/Discover";
 import Users from "./screens/Users";
 import Documents from './screens/Documents'
-// import Applications from './screens/Applications'
-// import Pages from './screens/Pages'
+import Applications from './screens/Applications'
+import Pages from './screens/Pages'
 
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
            <Route path="/discover" element={<Discover />} />
           <Route path="/users" element={<Users />} />
           <Route path="/documents" element={<Documents />} />
+          <Route path="/applications" element={<Applications />} />
+          <Route path="/pages" element={<Pages />} />
         </Routes>
-      </BrowserRouter> */}
-      hello
+      </BrowserRouter>
       
     </div>
   );
